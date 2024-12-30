93da0741b1c13e9ed394365222954d30ac13daeb
Dirk Stolle <striezel-dev@web.de>
2024-12-30 12:37:39 +0100

    add endpoint to set/update room-related client configuration data

--- Mocktrix/client/r0.6.1/ClientConfig.cs
+++ Mocktrix/client/r0.6.1/ClientConfig.cs
@@ -186,7 +186,7 @@ namespace Mocktrix.client.r0_6_1
         /// i. e. the endpoint to get account data for the client for a specific room.
         /// </summary>
         /// <param name="userId">id of the user to get account data for</param>
-        /// /// <param name="roomId">id of the room to which the account data belongs</param>
+        /// <param name="roomId">id of the room to which the account data belongs</param>
         /// <param name="type">event type of the account data to get</param>
         private static IResult GetRoomAccountDataByType(HttpContext context, string userId, string roomId, string type)
         {
@@ -236,6 +236,94 @@ namespace Mocktrix.client.r0_6_1
 
 
         /// <summary>
+        /// Implements https://spec.matrix.org/historical/client_server/r0.6.1.html#put-matrix-client-r0-user-userid-rooms-roomid-account-data-type,
+        /// i. e. the endpoint to set room-related account data for the client.
+        /// </summary>
+        /// <param name="userId">id of the user to get account data for</param>
+        /// <param name="roomId">id of the room to which the account data belongs</param>
+        /// <param name="type">event type of the account data to get</param>
+        private static async Task<IResult> SetRoomAccountDataByType(HttpContext context, string userId, string roomId, string type)
+        {
+            var access_token = Utilities.GetAccessToken(context);
+            if (string.IsNullOrWhiteSpace(access_token))
+            {
+                var error = new ErrorResponse
+                {
+                    errcode = "M_MISSING_TOKEN",
+                    error = "Missing access token."
+                };
+                return Results.Json(error, statusCode: StatusCodes.Status401Unauthorized);
+            }
+            var token = Database.Memory.AccessTokens.Find(access_token);
+            if (token == null)
+            {
+                var error = new ErrorResponse
+                {
+                    errcode = "M_UNKNOWN_TOKEN",
+                    error = "Unrecognized access token."
+                };
+                return Results.Json(error, statusCode: StatusCodes.Status401Unauthorized);
+            }
+            if (userId != token.user_id)
+            {
+                var error = new ErrorResponse
+                {
+                    errcode = "M_FORBIDDEN",
+                    error = "You cannot set account data of another user."
+                };
+                return Results.Json(error, statusCode: StatusCodes.Status403Forbidden);
+            }
+
+            // Server-managed types must be rejected, as per specification.
+            if (IsServerManagedType(type))
+            {
+                var error = new ErrorResponse
+                {
+                    errcode = "M_UNKNOWN",
+                    error = "The m.fully_read type cannot be set via this API."
+                };
+                return Results.BadRequest(error);
+            }
+
+            JsonNode? node;
+            try
+            {
+                node = JsonNode.Parse(await Utilities.GetRequestBodyAsString(context));
+            }
+            catch (Exception)
+            {
+                var error = new ErrorResponse
+                {
+                    errcode = "M_NOT_JSON",
+                    error = "The content is not valid JSON."
+                };
+                return Results.BadRequest(error);
+            }
+            if (node == null)
+            {
+                var error = new ErrorResponse
+                {
+                    errcode = "M_NOT_JSON",
+                    error = "The content is not valid JSON."
+                };
+                return Results.BadRequest(error);
+            }
+
+            var entry = Database.Memory.RoomConfigData.GetDatum(userId, roomId, type);
+            if (entry != null)
+            {
+                entry.Data = node;
+            }
+            else
+            {
+                _ = Database.Memory.RoomConfigData.Create(userId, roomId, type, node);
+            }
+
+            return Results.Ok(new { });
+        }
+
+
+        /// <summary>
         /// Adds endpoints for client configuration data to the web application.
         /// </summary>
         /// <param name="app">the app to which the endpoints shall be added</param>
@@ -246,12 +334,16 @@ namespace Mocktrix.client.r0_6_1
             app.MapGet("/_matrix/client/r0/user/{userId}/account_data/{type}", GetAccountDataByType);
 
             // Add https://spec.matrix.org/historical/client_server/r0.6.1.html#put-matrix-client-r0-user-userid-account-data-type,
-            // i. e. the endpoint to get account data for the client.
+            // i. e. the endpoint to set account data for the client.
             app.MapPut("/_matrix/client/r0/user/{userId}/account_data/{type}", SetAccountDataByType);
 
             // Add https://spec.matrix.org/historical/client_server/r0.6.1.html#get-matrix-client-r0-user-userid-rooms-roomid-account-data-type,
-            // i. e. the endpoint to get account data for the client.
+            // i. e. the endpoint to get room-related account data for the client.
             app.MapGet("/_matrix/client/r0/user/{userId}/rooms/{roomId}/account_data/{type}", GetRoomAccountDataByType);
+
+            // Add https://spec.matrix.org/historical/client_server/r0.6.1.html#put-matrix-client-r0-user-userid-rooms-roomid-account-data-type,
+            // i. e. the endpoint to set room-related account data for the client.
+            app.MapPut("/_matrix/client/r0/user/{userId}/rooms/{roomId}/account_data/{type}", SetRoomAccountDataByType);
         }
     }
 }
--- status-client-server-api-r0.6.1.md
+++ status-client-server-api-r0.6.1.md
@@ -415,13 +415,13 @@ TODO
 
 * ✅ 13.19.2.1 [PUT /_matrix/client/r0/user/{userId}/account_data/{type}](https://spec.matrix.org/historical/client_server/r0.6.1.html#put-matrix-client-r0-user-userid-account-data-type)
 * ✅ 13.19.2.2 [GET /_matrix/client/r0/user/{userId}/account_data/{type}](https://spec.matrix.org/historical/client_server/r0.6.1.html#get-matrix-client-r0-user-userid-account-data-type)
-* ❌ 13.19.2.3 [PUT /_matrix/client/r0/user/{userId}/rooms/{roomId}/account_data/{type}](https://spec.matrix.org/historical/client_server/r0.6.1.html#put-matrix-client-r0-user-userid-rooms-roomid-account-data-type)
+* ✅ 13.19.2.3 [PUT /_matrix/client/r0/user/{userId}/rooms/{roomId}/account_data/{type}](https://spec.matrix.org/historical/client_server/r0.6.1.html#put-matrix-client-r0-user-userid-rooms-roomid-account-data-type)
 * ✅ 13.19.2.4 [GET /_matrix/client/r0/user/{userId}/rooms/{roomId}/account_data/{type}](https://spec.matrix.org/historical/client_server/r0.6.1.html#get-matrix-client-r0-user-userid-rooms-roomid-account-data-type)
 
 #### 13.19.3 Server Behaviour
 
-🚧 Rejection of setting server-managed types is currently only implemented for the
-endpoint in 13.19.2.2.
+✅ The server prevents clients from setting account data for server-managed types
+  (currently only `m.fully_read`).
 
 ### 13.20 Server Administration
 
